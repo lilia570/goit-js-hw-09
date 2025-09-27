2ce18183cee137a73db33613e1b76510f85a62d4
lilia <liliapaslavska570@gmail.com>
2025-09-27 18:28:11 +0300

    correct

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,47 @@
-console.log('Form');
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const storageKey = 'feedback-form-state';
+
+ 
+const savedData = JSON.parse(localStorage.getItem(storageKey));
+if (savedData) {
+  formData.email = savedData.email ?? '';
+  formData.message = savedData.message ?? '';
+  Object.entries(formData).forEach(([key, value]) => {
+    if (form.elements[key]) form.elements[key].value = value;
+  });
+}
+
+ 
+form.addEventListener('input', onInput);
+form.addEventListener('submit', onSubmit);
+
+ 
+function onInput(event) {
+  const { name, value } = event.target;
+  if (formData[name] !== value.trim()) {
+    formData[name] = value.trim();
+    localStorage.setItem(storageKey, JSON.stringify(formData));
+  }
+}
+
+ 
+function onSubmit(event) {
+  event.preventDefault();
+  const { email, message } = form.elements;
+
+  if (!email.value.trim() || !message.value.trim()) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log({ ...formData });  
+  localStorage.removeItem(storageKey);  
+  form.reset();  
+  formData.email = '';
+  formData.message = '';
+}
